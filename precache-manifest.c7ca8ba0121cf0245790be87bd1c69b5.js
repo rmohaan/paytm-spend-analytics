47cd363874b8e2b5183fb372c90f5bc462e1e0ab
Mohaan Raja <rajam@advisory.com>
2020-08-22 17:31:26 +0530

    Updates

---
+++ precache-manifest.c7ca8ba0121cf0245790be87bd1c69b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e628430a6710029e371a1167e6814c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44d54f0f892281c582e6",
+    "url": "/static/css/main.3bfa9dea.chunk.css"
+  },
+  {
+    "revision": "b793aa9d44d404a00110",
+    "url": "/static/js/2.a0509bf8.chunk.js"
+  },
+  {
+    "revision": "c42689a5b69678dff0b0b528ab1b542a",
+    "url": "/static/js/2.a0509bf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44d54f0f892281c582e6",
+    "url": "/static/js/main.3f643383.chunk.js"
+  },
+  {
+    "revision": "50cf2e24a5bac156d014",
+    "url": "/static/js/runtime-main.4d81a670.js"
+  }
+]);
